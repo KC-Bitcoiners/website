b116d60d7e9e617a4d89ab7971f87d90194ac8f9
auggie <15633227+auggie-lahey@users.noreply.github.com>
2025-12-30 23:50:13 -0600

    Add new NPUB to the whitelist

--- src/config/whitelist.ts
+++ src/config/whitelist.ts
@@ -5,7 +5,8 @@ export const WHITELISTED_NPUBS = [
   "npub16ux4qzg4qjue95vr3q327fzata4n594c9kgh4jmeyn80v8k54nhqg6lra7",
   "npub1nrswn76gtr6apep95rev06y0cylk2t7utqyw9yn5x7qv8atgn3fscmpv2z",
   "npub187w4ykkurr3e89mm0rg5p49x6lveqtq0pp0um7qyk6xyrpeerx3s84exkz",
-  "npub1nv5c7sj2zxtjv7uayp2q2mneymm39mfp93wjhl5287y6yp6ey02qrsjhcn"
+  "npub1nv5c7sj2zxtjv7uayp2q2mneymm39mfp93wjhl5287y6yp6ey02qrsjhcn",
+  "npub1yvscx9vrmpcmwcmydrm8lauqdpngum4ne8xmkgc2d4rcaxrx7tkswdwzdu"
 ];
 
 // Convert npubs to hex for nostr relay filters
